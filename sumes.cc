7c10870c98d18eaaa2b08269de701d33652ae8af
PolRodr <polrodri11@gmail.com>
2021-11-11 18:41:48 +0100

    Sumo vectors

--- sumes.cc
+++ sumes.cc
@@ -6,8 +6,21 @@ using namespace std;
 
 
 int main ()
-{
-	int a, b;
-	cin >> a >> b;
-	cout << "Resultat : " << a+b << endl;
+{	
+	int a;
+	cin >> a;
+	vector <int> v(a);
+	for(int b=0; b < a; ++b) {
+		cin >> v[b]; }
+		int suma=0;
+	for(int c=0; c < a; ++c) {
+		suma+=v[c];
+
+
+	}
+	cout << suma << endl;
+
 }
+
+
+// llegir vector n i sumar totes les posicions del vector i penjar al github
